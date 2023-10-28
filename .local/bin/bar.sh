d689a605759435037f0d7bfd206868316f6379fc
PH <127600177+phonot@users.noreply.github.com>
2023-10-28 18:17:33 +0530

    Add files via upload

--- .local/bin/bar.sh
+++ .local/bin/bar.sh
@@ -1,1 +1,18 @@
+#!/bin/sh
+#check for volume
+update_volume(){
+	vol="$(pamixer --get-volume)"
+}
+update_volume
 
+#RTMIN = 34
+trap "update_volume" "RTMIN"
+
+#volume needs re write
+#vol | dd/mm/yy | day | hh/mm/ 
+while true;
+do
+	xsetroot -name "$vol | $(date +%d/%m/%y) | $(date +%a) | $(date +%I:%M%p)" && sleep 60
+
+done
+	
